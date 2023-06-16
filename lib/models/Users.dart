14f042f4734c4cac07f133e17159a5de0d7778c4
obaid <obaid@DESKTOP-4T09HNO>
2023-06-17 01:17:21 +0300

    fix

--- lib/models/Users.dart
+++ lib/models/Users.dart
@@ -1,9 +1,9 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:js_interop';
+
 
 import 'package:firebase_database/firebase_database.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:image_picker/image_picker.dart';
 import 'package:uuid/uuid.dart';
 
